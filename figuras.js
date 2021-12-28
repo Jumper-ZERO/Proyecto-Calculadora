3a49f72a8caa648afe4a0bb3c274a9625a776879
Jeremi Aron <chancanjeremiaron@gmail.com>
2021-12-28 11:36:06 -0500

    dando mensajes en la consola

--- figuras.js
+++ figuras.js
@@ -1,1 +1,62 @@
-console.log("Hello world");
+//codigo del cuadrado
+console.group("Cuadrado")
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + "cm^2");
+console.groupEnd();
+
+//codigo del triangulo
+console.group("Triangulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log(
+    "Los lados del triangulo miden: " 
+    + ladoTriangulo1 
+    + "cm, " 
+    + ladoTriangulo2
+    + "cm, "
+    + baseTriangulo 
+    + "cm"
+    );
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triangulo es: " + alturaTriangulo + "cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perimetro del triangulo es: " + perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El area del triangulo es: " + areaTriangulo + "cm^2");
+
+console.groupEnd();
+
+//codigo de Circulo
+console.group("Circulos");
+
+// Radio
+const radioCirculo = 4;
+console.log("El radio del circulo es: " + radioCirculo + "cm");
+
+// Diametro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo es: " + diametroCirculo + "cm");
+
+// PI
+const piCirculo = Math.PI;
+console.log("El PI del circulo es: " + piCirculo + "cm");
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * piCirculo;
+console.log("La circunferencia del circulo es: " + perimetroCirculo + "cm");
+
+// Area
+const areaCirculo = (radioCirculo * radioCirculo) * piCirculo;
+console.log("El area del circulo es: " + areaCirculo + "cm^2");
+
+console.groupEnd();
