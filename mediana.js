7fd4bff572504f42dcf6c6886a19a4abe85cf896
Jeremi Aron <chancanjeremiaron@gmail.com>
2021-12-30 22:13:29 -0500

    sacar la mediana y ordenar la lista

--- mediana.js
+++ mediana.js
@@ -1,20 +1,26 @@
-function calcularMediaAritmetica(lista){
-    const sumarLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElemento) {
-            return valorAcumulado + nuevoElemento;
-        }
-    );
-    const promedioLista = sumarLista / lista.length;
-    return promedioLista;
+let mediana;
+
+function Conversor(){
+    const NumeroSinOrdenar = document.getElementById("Numeros").value.split(','); //Split() es para crear un array con el separador de , 
+    const Numero = NumeroSinOrdenar.sort(); //Esto es para ordenar el Array
+    const resultado = document.getElementById("Resultado"); //Para poner un parrafo con el resultado
+    const mitadNumeros = parseInt(Numero.length / 2);//Saber donde es la mitad
+
+    if (esPar(Numero.length)) {
+        const elemento1 = parseInt(Numero[mitadNumeros - 1]); //la posicion uno del medio y un parseInt para que sea un numero entero
+        const elemento2 = parseInt(Numero[mitadNumeros]);     //la posicion dos del medio
+
+        const promedioElemento1y2 = calcularMediaAritmetica([elemento1,elemento2]); //ver la media de los elementos pares
+
+        resultado.innerHTML = promedioElemento1y2;
+
+    } else {
+    mediana = Numero[mitadNumeros];
+    resultado.innerHTML = mediana;
+    }
 };
 
-const lista1 = [
-    100,
-    200,
-    500,
-    400000000,
-];
-const mitadLista1 = parseInt(lista1.length / 2);
+//______Para saber si es par
 
 function esPar(numerito) {
     if (numerito % 2 === 0) {
@@ -24,20 +30,15 @@ function esPar(numerito) {
     }
 }
 
-let mediana;
 
-if (esPar(lista1.length)) {
-    const elemento1 = lista1[mitadLista1 - 1];
-    const elemento2 = lista1[mitadLista1];
+//______Media Aritmetica
 
-    const promedioElemento1y2 = calcularMediaAritmetica([ 
-        elemento1,
-        elemento2
-    ]);
-
-    mediana = promedioElemento1y2;
-
-} else {
-    mediana = lista1[mitadLista1];
-
-}
+function calcularMediaAritmetica(lista){
+    const sumarLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+    const promedioLista = sumarLista / lista.length;
+    return promedioLista;
+};
