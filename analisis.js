3f54b734634902265a4d1ca1ae73cec093ee4794
Jeremi Aron <chancanjeremiaron@gmail.com>
2022-02-10 12:52:57 -0500

    cambiando a arrow funtions y comentarios

--- analisis.js
+++ analisis.js
@@ -1,11 +1,7 @@
 //Variables con valore de la lista
-const salariosCol = colombia.map(personsita => personsita.salary)
+const salariosCol = colombia.map(({salary}) => salary)
 
-const salariosColSorted = salariosCol.sort(
-    function (a, b) {
-        return a - b;
-    }
-);
+const salariosColSorted = salariosCol.sort((a, b) => a - b);
 
 // Funciones para LLamar
 function esPar(numerito) {
@@ -13,24 +9,20 @@ function esPar(numerito) {
 }
 
 function calcularMediaAritmetica(lista) {
-    const sumaLista = lista.reduce(
-        function(valorAcumulado = 0, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
-        }
-        );
+    const sumaLista = lista.reduce((a = 0, b) => a + b);
         const promedioLista = sumaLista / lista.length; 
         return promedioLista; 
 }
 
 //Proceso
 function medianaSalarios(lista) {
-    const mitad = parseInt(lista.length / 2);
+    const mitad = parseInt(lista.length / 2); //super necesario
 
-    if (esPar(lista.length)) {
+    if (esPar(lista.length)) { //cuales son los indices del medio en el arreglo
         const personitaMitad1 = lista[mitad - 1];
         const personitaMitad2 = lista[mitad];
 
-        const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
+        const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]); //la funcion solo acepta una arreglo []
         return mediana;
     } else {
         const personitaMitad = lista[mitad];
@@ -38,5 +30,5 @@ function medianaSalarios(lista) {
     }
 }
 console.log(
-    medianaSalarios(salariosColSorted)
+    medianaSalarios(salariosColSorted) // la funcion y la lista
 )
